6f149fc347236cff8ca2d63b8fec84f038b68fa3
Karim <karimovkarim1902@gmail.com>
2021-08-29 14:52:43 +0500

    No this is My change

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/cahnge',function(){
-   return dd("My change") ;
+   return dd("No this is My change") ;
 });
